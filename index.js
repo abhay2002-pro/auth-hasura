6ab94ec41a6ecc7f0653e251ed88b0a59f3515f0
Abhay Ray <abhayray2002@gmail.com>
2024-03-19 22:07:43 +0530

    change

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.use(express.json())
 
 app.use("/", routes)
 
+app.get("/health", (req, res)=> {
+    res.status(200).json({
+        "message": "success"
+    })
+})
+
 app.listen(3000, () => {
     console.log("listening on 3000")
 })
