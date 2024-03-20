bf9a8d0b9daef22a7e3937d4ba7846cc24f7e47f
Abhay Ray <abhayray2002@gmail.com>
2024-03-20 20:34:43 +0530

    update JWT

--- routes.js
+++ routes.js
@@ -83,6 +83,7 @@ router.post('/SignIn', async (req, res) => {
       { "https://hasura.io/jwt/claims": {
           "x-hasura-admin-secret": "3MW1mI82c0G744EFNV3V91Qf8Uo41B9qBkp0TbvgrdHoLJjHniiKYA5Iop7y9qfG",
           "x-hasura-user-id": data.users[0].id,
+          " x-hasura-allowed-roles":  ["admin", "user"],
           "x-hasura-role": "user" 
         }
       }, 
